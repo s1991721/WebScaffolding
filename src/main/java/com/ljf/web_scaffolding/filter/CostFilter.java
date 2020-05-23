b7d0aed4490ce42afa70d05193d5d4cf506b39d3
mr.lin <278971585@qq.com>
2020-05-23 16:43:30 +0800

    统计接口响应时间 过滤器

---
+++ src/main/java/com/ljf/web_scaffolding/filter/CostFilter.java
@@ -0,0 +1,25 @@
+package com.ljf.web_scaffolding.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Created by mr.lin on 2020/5/23
+ * 统计接口响应时间
+ */
+@Slf4j
+@Component
+public class CostFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        long start = System.currentTimeMillis();
+        chain.doFilter(request, response);
+        // TODO: 2020/5/23 输出接口名 
+        log.info("cost time:{}ms", System.currentTimeMillis() - start);
+    }
+
+}
